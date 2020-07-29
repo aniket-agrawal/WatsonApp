a3138035236e77f604babce8cb9dd94a4f75e0d0
aniket <aniket9636541817@gmail.com>
2020-07-29 22:57:41 +0530

    connection corrected

--- app/src/main/java/com/example/watsonapp/SplashActivity.java
+++ app/src/main/java/com/example/watsonapp/SplashActivity.java
@@ -1,17 +1,23 @@
 package com.example.watsonapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AppOpsManager;
 import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserInfo;
@@ -29,6 +35,7 @@ import static com.example.watsonapp.FrontActivity.USAGE_MIN;
 public class SplashActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +48,7 @@ public class SplashActivity extends AppCompatActivity {
             finish();
         } else {
 //            zoom();
-
+            usage();
         /*SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         Gson gson = new Gson();
 
@@ -58,23 +65,27 @@ public class SplashActivity extends AppCompatActivity {
         editor.putString(USAGE_MIN, json1);
         editor.putString(USAGE_HOUR, json1);
         editor.apply();*/
-            mAuth = FirebaseAuth.getInstance();
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startService(new Intent(SplashActivity.this, BackgroundService.class));
-                    if (mAuth.getCurrentUser() == null) {
-                        Intent i = new Intent(SplashActivity.this, SignInUpActivity.class);
-                        startActivity(i);
-                        finish();
-                    } else {
-                        startActivity(new Intent(SplashActivity.this, FrontActivity.class));
-                        finish();
-                    }
-                }
-            }, 2000);
+        mAuth = FirebaseAuth.getInstance();
         }
     }
+
+    private void call(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startService(new Intent(SplashActivity.this, BackgroundService.class));
+                if (mAuth.getCurrentUser() == null) {
+                    Intent i = new Intent(SplashActivity.this, SignInUpActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    startActivity(new Intent(SplashActivity.this, FrontActivity.class));
+                    finish();
+                }
+            }
+        }, 2000);
+    }
+
 
 //    private void zoom() {
 //        ImageView image = (ImageView)findViewById(R.id.imageView);
@@ -83,4 +94,35 @@ public class SplashActivity extends AppCompatActivity {
 //                        R.anim.zoomin);
 //        image.startAnimation(animation1);
 //    }
+
+    public void usage() {
+        AppOpsManager appOps = (AppOpsManager)
+                getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getPackageName());
+        if(mode == AppOpsManager.MODE_ALLOWED){
+            call();
+        }
+        else{
+            startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS){
+            AppOpsManager appOps = (AppOpsManager)
+                    getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(), getPackageName());
+            if(mode == AppOpsManager.MODE_ALLOWED){
+                call();
+            }
+            else{
+                Toast.makeText(SplashActivity.this,"This APP is not for you." , Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
